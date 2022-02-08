ccc6b0a9a4d610e489437b8cf37df07f86a92ec8
Francois Spies <fcspies@gmail.com>
2022-02-08 09:26:04 -0500

    go

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,7 +1,10 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants}) {
+
+  const listedPlants = plants.map(plant => <PlantCard key={plant.name plant=>{plant}/>)
+
   return (
     <ul className="cards">{/* render PlantCards components in here */}</ul>
   );
