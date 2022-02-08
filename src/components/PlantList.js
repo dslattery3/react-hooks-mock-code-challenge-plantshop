8a4bf7118e5e52248d0b650ceca7878e2671541c
Francois Spies <fcspies@gmail.com>
2022-02-08 09:26:52 -0500

    go

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,7 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList({plants}) {
-
-  const listedPlants = plants.map(plant => <PlantCard key={plant.name plant=>{plant}/>)
-
+function PlantList() {
   return (
     <ul className="cards">{/* render PlantCards components in here */}</ul>
   );
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,25 +1,14 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
-
-  const [plants, setPlants] = useState([])
-
-  useEffect(()=>{
-    fetch("http://localhost:6001/plants")
-    .then(r=>r.json())
-    .then(data=> setPlants(data))
-  },[])
-
-  console.log(plants)
-
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList plants={plants}/>
+      <PlantList />
     </main>
   );
 }
