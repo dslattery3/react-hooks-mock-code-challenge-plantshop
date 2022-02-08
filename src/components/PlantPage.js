aaa58ef0f35ddb8f7fce6bf8f2975bbbe88a6326
Francois Spies <fcspies@gmail.com>
2022-02-08 10:05:15 -0500

    here it is

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [addPlant, setAddPlant] = useState("")
+  const [addImage, setAddimage] = useState("")
+  const [addPrice, setAddPrice] = useState(0)
+
+  function handleNameChange(e){
+    setAddPlant(e.target.value)
+  }
+
+  function handleImageChange(e) {
+    setAddimage(e.target.value)
+  }
+
+  function handlePriceChange(e) {
+    setAddPrice(e.target.value)
+  }
+
+  function submitForm() {
+  
+      fetch("http://localhost:6001/plants", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          "name" : "addPlant",
+          "image" : "addImage",
+          "price" : addPrice
+        })
+      })
+      .then(r=>r.json())
+      .then(data => console.log(data))
+
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+      .then(r=>r.json())
+      .then(data => setPlants(data))
+
+  }, [])
+
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm handleNameChange={handleNameChange} handleImageChange={handleImageChange} handlePriceChange={handlePriceChange} submitForm={submitForm}/>
       <Search />
-      <PlantList />
+      <PlantList plants={plants} />
     </main>
   );
 }
